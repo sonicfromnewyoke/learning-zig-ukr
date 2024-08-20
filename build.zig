7f6810e26a21232445855f4fcd1a0ff376e785d1
Sonic <sonic.from.new.yoke@gmail.com>
2024-08-21 00:34:32 +0300

    fix relative path

--- build.zig
+++ build.zig
@@ -3,8 +3,9 @@ const zine = @import("zine");
 
 pub fn build(b: *std.Build) !void {
     zine.website(b, .{
-        .title = "Learn Zig UKR",
-        .host_url = "https://sample.com",
+        .title = "Learning Zig UKR",
+        .host_url = "https://sonicfromnewyoke.github.io/",
+        .url_path_prefix = "learning-zig-ukr",
         .content_dir_path = "content",
         .layouts_dir_path = "layouts",
         .assets_dir_path = "assets",
